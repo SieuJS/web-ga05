c135dac09923a9e4dc02eb9147726c667de69221
21120321 <gamefun206@gmail.com>
2024-11-30 16:40:08 +0700

    addjust type for serialize

--- src/modules/auth/provider/session.serializer.ts
+++ src/modules/auth/provider/session.serializer.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@nestjs/common"
 import { PassportSerializer } from "@nestjs/passport"
+import { UserInSession } from "../../user"
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  serializeUser(user: any, done: (err: Error | null, user: any) => void): any {
+  serializeUser(user: UserInSession, done: (err: Error | null, user: UserInSession) => void): any {
     done(null, user)
   }
   deserializeUser(
